f71cb3acc0891bb28478d46fbad1b404c5c4a35a
ETSTR <Etsgaming4216@gmail.com>
2026-08-16 09:23:21 +0300

    Add service worker event listeners for install and activate

---
+++ sw.js
@@ -0,0 +1,7 @@
+self.addEventListener('install', (e) => { self.skipWaiting(); });
+self.addEventListener('activate', (e) => { return self.clients.claim(); });
+
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(clients.openWindow('/'));
+});
